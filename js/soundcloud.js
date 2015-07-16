b23c20a9e2ef83bab323b7a23de2a4a434a85a25
George Urick <George@Georges-MacBook-Pro.local>
2015-07-15 23:36:10 -0700

    button in top right

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,17 +1,38 @@
 
+// function play() {
+//     $(document).ready(function() {
+//                 var widgetIframe = document.getElementById('sc-widget'),
+//                 widget = SC.Widget(widgetIframe);
+
+//                 widget.bind(SC.Widget.Events.READY, function() {
+//                 console.log('ready');
+//                 widget.toggle()
+
+//                 var playButton = document.getElementById('i.play_button');
+//                 if (playButton.hasClass('fa-play-circle')) {
+//                     playButton.removeClass('fa-play-circle');
+//                     playButton.addClass('fa-pause');
+//                 }
+//                 else {
+//                     playButton.removeClass('fa-pause');
+//                     playButton.addClass('fa-play-circle');
+//                 }
+
+//             });
+//         });
+//     }
+
+//     document.getElementById('play_button').onclick = play;
 
 function play() {
-	$(document).ready(function() {
-                var widgetIframe = document.getElementById('sc-widget'),
-                widget = SC.Widget(widgetIframe);
-
-                widget.bind(SC.Widget.Events.READY, function() {
-                console.log('ready');
-                widget.play();
-                });
-                // $('playButton').click(function() {
-                    widget.play();
-
-                // });
-            });
+    var playButton = document.getElementById('i.play_button');
+    if (playButton.hasClass('fa-play-circle')) {
+      playButton.removeClass('fa-play-circle');
+      playButton.addClass('fa-pause');
+    } else {
+      playButton.removeClass('fa-pause');
+      playButton.addClass('fa-play-circle');
+    }
 }
+
+document.getElementById('play_button').onclick = play;

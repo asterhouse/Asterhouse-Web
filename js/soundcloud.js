5e61c051e3ef84223200bc7da32674039cd4acf0
Finn <finn@finn.io>
2015-07-15 23:59:45 -0700

    Fixed the play button a bit, required significant re-ordering
    * JS gets loaded at the bottom of the page, always
    * JS line comments don't work in HTML, only JS
    * The Google maps line was causing an error so I commented it for now
    * Reworked the play function into an anonymous function
    * Made the play button properly reference the play/pause icon
    * made the play button binding correctly reference the play button so it actully triggers

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -24,15 +24,15 @@
 
 //     document.getElementById('play_button').onclick = play;
 
-function play() {
-    var playButton = document.getElementById('i.play_button');
+$('.play-button-link').click(function() {
+    var playButton = $('.play-button');
     if (playButton.hasClass('fa-play-circle')) {
+      console.log('Playing music');
       playButton.removeClass('fa-play-circle');
       playButton.addClass('fa-pause');
     } else {
+      console.log('Stopping music');
       playButton.removeClass('fa-pause');
       playButton.addClass('fa-play-circle');
     }
-}
-
-document.getElementById('play_button').onclick = play;
+});
